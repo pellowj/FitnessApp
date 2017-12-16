5a0f495d83cf6cdf4a5df46e2377c50796d2b8d5
John Pellow <pellowj@aol.com>
2017-12-15 23:29:59 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FitnessApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user for minutes exercised
+            Console.Write("Enter how many minutes you excercised:  ");
+
+            // Add minutes exercised to total
+
+            // Display total minutes exercised to the screen
+
+            // Repeat until the user quits
+
+
+
+
+
+            // Keep console window open for testing
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
